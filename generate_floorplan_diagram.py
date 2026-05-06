46f9b41e9fdd79e7314f6627eb6d4a5e7c17311d
alvinr <alvin.richards@mac.com>
2026-05-06 11:29:15 -0700

    Revert "Floorplan sheet 2: shift diagram up, add 50% top padding for title separation"
    This reverts commit 8fac18dc02cb838dc4e9e6c7b970a6310a04426d.

--- generate_floorplan_diagram.py
+++ generate_floorplan_diagram.py
@@ -466,10 +466,7 @@ def egress_detail():
     # Extend X range leftward to show panel in open (exterior) position
     # Panel is 2362mm wide; when open 180° the far tip reaches X = -2362
     X_LO, X_HI = -2700, 1200
-    # Extra top padding = 50% of content height, for title block separation
-    CONTENT_H = C_WID + 2 * PAD   # ~3062mm
-    TOP_PAD = PAD + int(CONTENT_H * 0.50)   # 350 + 1531 ≈ 1881mm
-    Y_LO, Y_HI = -PAD, C_WID + TOP_PAD
+    Y_LO, Y_HI = -PAD, C_WID + PAD
     FIG_W = 22.0
     FIG_H = FIG_W * (Y_HI - Y_LO) / (X_HI - X_LO)
 

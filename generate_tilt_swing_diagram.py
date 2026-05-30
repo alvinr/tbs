95c57bd740365fda6b5e0c4157ec9cd54585533b
alvinr <alvin.richards@mac.com>
2026-05-29 18:42:04 -0700

    Tilt-swing sheet 1: shift diagram up 50mm for more notes clearance

--- generate_tilt_swing_diagram.py
+++ generate_tilt_swing_diagram.py
@@ -108,7 +108,7 @@ def draw_sheet1():
     draw_rect(ax, 15, 15, pw - 30, ph - 30, lw=1.5, color='black', fc='white')
 
     cx = pw / 2
-    cy = ph / 2 + 30
+    cy = ph / 2 + 55
 
     def s(mm):
         return mm / SC
